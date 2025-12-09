e20aa95ae80aa041061d6ace92bc155ce2f9eba7
daniellauding <daniel@lauding.se>
2025-12-09 12:05:19 +0100

    api works

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-// Import required  hooks
+import { useState, useEffect } from "react";
 import { DogFact } from "./components/DogFact";
 
 export const App = () => {
   // Hint: Initialize state for storing the dog fact
+  const [dogFact, setDogFact] = useState([]);
+
   // Hint: Define the API endpoint
+  const LIMIT = `?limit=5`
+  const API = `https://dogapi.dog/api/v2/facts${LIMIT}`;
+  
   // Hint: Create a function to fetch the dog fact
+  const fetchDogFact = async () => {
+    const response = await fetch(API)
+    const result = await response.json();
+    console.log('pure:', response)
+    console.log('response:', result)
+    setDogFact(result.data);
+  } 
+
   // Hint: Use the useEffect hook to fetch the dog fact when the component mounts
+  useEffect(() => {
+    fetchDogFact()
+  }, []);
 
   return (
     <div className="App">
-      <DogFact />
+      {dogFact.map((fact) => (
+        // <DogFact key={fact.id} fact={fact.attributes.body} />
+        <DogFact key={fact.id} fact={fact} />
+      ))}
     </div>
   );
 };
--- src/components/DogFact.jsx
+++ src/components/DogFact.jsx
@@ -1,10 +1,30 @@
 // DogFact Component
-export const DogFact = () => {
+// import {useState, useEffect} from 'react'
+
+export const DogFact = ({ fact }) => {
+  // const [facts, setFacts] = useState()
+  // const body = result.data.attributes.body
+
+  // useEffect(() => {
+    console.log(fact);
+  // })
   // Hint: Extract the 'body' from 'attributes' of the 'fact' prop
 
   // Hint: Render the 'cleanFact' if available, otherwise show a loading message
-  return <div>dog fact here.</div>;
+  // return <div>{fact} dog fact here.</div>;
+return <div>{fact.attributes.body} dog fact here.</div>;
 };
 
 // Hint: To use this component, import it in your main App component and pass the 'fact' prop to it.
 // Example: <DogFact fact={yourFactData} />
+
+
+// [
+//     {
+//         "id": "20b4826f-28d8-4cf2-aa26-7f66230e498d",
+//  -–––>  "type": "fact",
+//  –––>   "attributes": {
+//          -–––> "body": "A dog's nose is wet because it is lined with mucus, which helps to pick up scents."
+//         }
+//     },
+// ]
